29054129157906eb367720d08d5731642185e823
imsofial <sofisolsof@gmail.com>
2024-07-06 23:30:07 +0300

    actions addition

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+const isProd = process.env.NODE_ENV === 'production';
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
+module.exports = withPWA({
+  basePath: '/your-repo-name',
+  assetPrefix: isProd ? '/your-repo-name/' : '',
+  trailingSlash: true,
+});
+
+
 export default nextConfig;
